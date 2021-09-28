07436d31d42cc950b7c52b3df608450de604a580
shangmengmeng <793514513@qq.com>
2021-09-28 13:53:51 +0800

    第二次提交

--- starLoading/src/main/java/com/star/starloading/star_dialog/StarLoadingDialog.kt
+++ starLoading/src/main/java/com/star/starloading/star_dialog/StarLoadingDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
